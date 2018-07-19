991c957b17769b11c00a71058f364acaa2bebe5d
Leonora20 <leonorafernandes20@gmail.com>
2018-07-19 16:50:02 +0530

    multiple_Inheritance

--- EmpManagement/src/Subject.java
+++ EmpManagement/src/Subject.java
@@ -1,13 +1,10 @@
 
 public class Subject extends TeachingStaff {
 
-	public static void main(String[] args) {
-		System.out.println("teaches subjects");
-	}
-
 	@Override
 	public void teach() {
 		super.teach();
+		System.out.println("teaches subjects");
 	}
 
 }
